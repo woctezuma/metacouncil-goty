8287be8667310f470425dc0faf27e40abb99469a
Wok <woctezuma@users.noreply.github.com>
2019-01-05 12:01:11 +0100

    Additional hard-coded matches for optional categories (DLC, etc.)

--- hard_coded_matches.py
+++ hard_coded_matches.py
@@ -5,13 +5,15 @@ def get_hard_coded_app_id_dict():
     # Hard-coded list of game names which are wrongly matched with Levenshtein distance
 
     hard_coded_dict = {
+        # Manually fix matches for which the automatic name matching is wrong:
         "The Missing": "842910",
         "Pillars of Eternity 2": "560130",
         "Dragon Quest XI: Shadows of an Elusive Age": "742120",
         "Atelier Rorona ~The Alchemist of Arland~ DX": "936160",
 
-        # Add an entry for a game which we could not manually find on Steam. We start with -1, and decrement from there.
+        # Add an entry for each game which could not be found on Steam. Start with -1, and decrement from there.
         "Spider-Man": "-1",
+        "Fallout 76": "-2",
     }
 
     return hard_coded_dict
@@ -19,9 +21,20 @@ def get_hard_coded_app_id_dict():
 
 def get_hard_coded_steamspy_database_extension():
     database_extension = {
+        # Games which could not be found on Steam. Typically, games from other PC stores, or on consoles:
         "-1": {
             "name": "Marvel's Spider-Man", "developer": "Insomniac Games", "publisher": "Sony Interactive Entertainment"
         },
+        "-2": {
+            "name": "Fallout 76", "developer": "Bethesda Game Studios", "publisher": " Bethesda Softworks"
+        },
+
+        # Steam DLC, which are not provided by SteamSpy:
+        "624621": {"name": "Wolfenstein II: The Freedom Chronicles - Episode 2", },
+        "744840": {"name": "Rainbow Six Siege - Year 3 Pass", },
+        "865670": {"name": "Prey - Mooncrash", },
+        "911930": {"name": "BATTLETECH Flashpoint", },
+        "947950": {"name": "Faeria - Puzzle Pack Elements", },
     }
 
     return database_extension
