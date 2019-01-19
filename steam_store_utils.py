134cbc7730c2bb02abd8f73794524548cc02c0fe
Wok <woctezuma@users.noreply.github.com>
2019-01-19 22:22:17 +0100

    More precise argument name

--- steam_store_utils.py
+++ steam_store_utils.py
@@ -1,10 +1,10 @@
-def get_link_to_store(app_id, remove_dummy_app_id=True):
+def get_link_to_store(app_id, hide_dummy_app_id=True):
     steam_store_base_url = 'https://store.steampowered.com/app/'
 
     if int(app_id) > 0:
         link_to_store = '[URL=' + steam_store_base_url + app_id + '/]' + app_id + '[/URL]'
     else:
-        if remove_dummy_app_id:
+        if hide_dummy_app_id:
             link_to_store = 'n/a'
         else:
             link_to_store = app_id
