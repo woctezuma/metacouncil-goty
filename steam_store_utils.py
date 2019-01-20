5b74332bdb360756efffa340dbffcc85fae80712
Wok <woctezuma@users.noreply.github.com>
2019-01-20 18:56:24 +0100

    Function to check whether a Steam appID is tagged as Early Access

--- steam_store_utils.py
+++ steam_store_utils.py
@@ -1,3 +1,6 @@
+import steampi.api
+
+
 def get_link_to_store(app_id, hide_dummy_app_id=True):
     steam_store_base_url = 'https://store.steampowered.com/app/'
 
@@ -9,3 +12,17 @@ def get_link_to_store(app_id, hide_dummy_app_id=True):
         else:
             link_to_store = app_id
     return link_to_store
+
+
+def get_early_access_status(app_id):
+    if int(app_id) > 0:
+        app_details, _, _ = steampi.api.load_app_details(app_id)
+    else:
+        app_details = {}
+
+    try:
+        is_early_access = bool(any(genre['description'] == 'Early Access' for genre in app_details['genres']))
+    except KeyError:
+        is_early_access = False
+
+    return is_early_access
