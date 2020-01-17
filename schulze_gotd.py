e9982599313d1d81115dfb27067a4028fe481505
Wok <woctezuma@users.noreply.github.com>
2020-01-17 18:46:15 +0100

    WIP Game of the Decade (GotD)

---
+++ schulze_gotd.py
@@ -0,0 +1,31 @@
+from schulze_goty import apply_pipeline
+
+if __name__ == '__main__':
+    ballot_year = '2019'
+    input_filename = 'pc_gaming_metacouncil_goty_awards_' + ballot_year + '.csv'
+    use_igdb = True
+    retrieve_igdb_data_from_scratch = False
+    apply_hard_coded_extension_and_fixes = True
+    use_levenshtein_distance = True
+
+    # Game of the Decade
+    # Caveat: pay attention to the d in 'gotd'.
+    goty_field = 'gotd_preferences'
+    # Either:
+    duration_in_years = 10
+    release_year = str(int(ballot_year) - duration_in_years + 1)
+    year_constraint = 'minimum'
+    # Or:
+    # release_year = None
+    # year_constraint = None
+
+    apply_pipeline(input_filename,
+                   release_year=release_year,
+                   fake_author_name=False,
+                   try_to_break_ties=False,
+                   use_igdb=use_igdb,
+                   retrieve_igdb_data_from_scratch=retrieve_igdb_data_from_scratch,
+                   apply_hard_coded_extension_and_fixes=apply_hard_coded_extension_and_fixes,
+                   use_levenshtein_distance=use_levenshtein_distance,
+                   goty_field=goty_field,
+                   year_constraint=year_constraint)
