b7d1073f529e96dac227ac1b74f6033b43cd8d31
Wok <woctezuma@users.noreply.github.com>
2021-01-26 14:34:01 +0100

    Split parse_csv() into smaller functions

--- parsing_utils.py
+++ parsing_utils.py
@@ -40,41 +40,64 @@ def parse_csv(fname, params):
     for line in text_data:
         tokens = [token.strip(quote) for token in line.split(";")]
 
-        ind = indices["voter_name"]
-        voter_name = tokens[ind]
+        voter_name = read_voter_name(tokens, indices)
 
-        ballots[voter_name] = dict()
+        single_ballot = dict()
+        single_ballot = fill_in_review(tokens, indices, single_ballot=single_ballot)
+        single_ballot = fill_in_game_list(tokens, indices, params, single_ballot=single_ballot)
+        single_ballot = fill_in_best_optional(single_ballot=single_ballot)
 
-        for categorie in get_categories("main"):
-            ind = indices["review"][categorie]
-            if ind is None:
-                review = ""
-            else:
-                review = tokens[ind]
-
-            goty_review_field = f"{categorie}_description"
-            ballots[voter_name][goty_review_field] = review
-
-        for categorie_type in ["main", "optional"]:
-            for categorie in get_categories(categorie_type=categorie_type):
-                ind_list = indices[categorie_type][categorie]
-                d = extract_tokens(tokens, ind_list, params[categorie]["num_choices"])
-
-                goty_field = f"{categorie}_preferences"
-                ballots[voter_name][goty_field] = d
-
-        for categorie in get_categories("optional"):
-            goty_field = f"{categorie}_preferences"
-            best_position = 1
-            try:
-                best_game = ballots[voter_name][goty_field][best_position]
-            except KeyError:
-                best_game = None
-
-            best_field = f"best_{categorie}"
-            ballots[voter_name][best_field] = best_game
+        ballots[voter_name] = single_ballot
 
     return ballots
+
+
+def read_voter_name(tokens, indices):
+    ind = indices["voter_name"]
+    voter_name = tokens[ind]
+
+    return voter_name
+
+
+def fill_in_review(tokens, indices, single_ballot):
+    for categorie in get_categories("main"):
+        ind = indices["review"][categorie]
+        if ind is None:
+            review = ""
+        else:
+            review = tokens[ind]
+
+        goty_review_field = f"{categorie}_description"
+        single_ballot[goty_review_field] = review
+
+    return single_ballot
+
+
+def fill_in_game_list(tokens, indices, params, single_ballot):
+    for categorie_type in ["main", "optional"]:
+        for categorie in get_categories(categorie_type=categorie_type):
+            ind_list = indices[categorie_type][categorie]
+            d = extract_tokens(tokens, ind_list, params[categorie]["num_choices"])
+
+            goty_field = f"{categorie}_preferences"
+            single_ballot[goty_field] = d
+
+    return single_ballot
+
+
+def fill_in_best_optional(single_ballot):
+    for categorie in get_categories("optional"):
+        goty_field = f"{categorie}_preferences"
+        best_position = 1
+        try:
+            best_game = single_ballot[goty_field][best_position]
+        except KeyError:
+            best_game = None
+
+        best_field = f"best_{categorie}"
+        single_ballot[best_field] = best_game
+
+    return single_ballot
 
 
 if __name__ == "__main__":
