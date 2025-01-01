322b290701bb05632b370929f07438638b78c2fe
Wok <woctezuma@users.noreply.github.com>
2025-01-01 18:22:42 +0100

    Try to load secrets from the environment
    This may help to fix unit tests which are fine on my local machine, but fail on GitHub.

--- igdb_local_secrets.py
+++ igdb_local_secrets.py
@@ -1,5 +1,10 @@
 import json
+import os
 from pathlib import Path
+
+
+def get_environment() -> dict:
+    return os.environ.copy()
 
 
 def get_igdb_user_key_file_name() -> str:
@@ -9,6 +14,8 @@ def get_igdb_user_key_file_name() -> str:
 def load_igdb_user_key() -> dict:
     file_name = get_igdb_user_key_file_name()
 
+    environment = get_environment()
+
     try:
         with Path(file_name).open(encoding="utf-8") as f:
             igdb_user_key = json.load(f)
@@ -16,10 +23,10 @@ def load_igdb_user_key() -> dict:
         print(f"IGDB user secret key {file_name} not found.")
         igdb_user_key = {
             # For version 3 of IGDB API:
-            "user-key": "",
+            "user-key": environment.get("IGDB_USER_KEY", ""),
             # For version 4 of IGDB API:
-            "client_id": "",
-            "client_secret": "",
+            "client_id": environment.get("IGDB_CLIENT_ID", ""),
+            "client_secret": environment.get("IGDB_CLIENT_SECRET", ""),
             # The following are temporary credentials which should be obtained with the pair (id, secret) above:
             "token_type": "",
             "access_token": "",
